7c6750db11defc1428079220f45560e71327b8e4
Ana.Sousa <anasousa314@outlook.com>
2021-03-14 21:14:54 -0300

    Adciona index e create em ToDoListController

--- controllers/ToDoListController.js
+++ controllers/ToDoListController.js
@@ -1,2 +1,27 @@
+
 const {Activity} = require('../models');
 
+module.exports = {
+  async index(req,res,next){
+    let activities = await Activity.findAll();
+
+    res.render('index',{activities});
+  },
+
+  async create(req,res,next){
+    let activity = {...req.body};
+
+    await Activity.create(activity);
+
+    console.log(activity)
+    res.redirect('/');
+  },
+
+  async edit(req,res,next){
+    let id = req.params.id;
+
+    let activity = await Activity.findByPk(id);
+
+
+  }
+}
