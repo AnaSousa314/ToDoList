c321ab8480a8c16840be7afefe3a9d5c3eaf5140
Ana.Sousa <anasousa314@outlook.com>
2021-04-08 12:18:42 -0300

    Modifica as rotas index, create e edit, e adiciona as rotas update e delete

--- controllers/ToDoListController.js
+++ controllers/ToDoListController.js
@@ -13,15 +13,42 @@ module.exports = {
 
     await Activity.create(activity);
 
-    console.log(activity)
+    //console.log(activity)
     res.redirect('/');
   },
 
   async edit(req,res,next){
     let id = req.params.id;
 
+    console.log(id);
+
     let activity = await Activity.findByPk(id);
 
+    console.log(activity)
+    res.render('edit-task',{activity});
+  },
 
+  async updated(req,res,next){
+    let id = req.params.id;
+
+    let {task} = req.body;
+
+    let activity = await Activity.findByPk(id);
+
+    activity.task = task;
+
+    await activity.save();
+
+    res.render('edit-task',{activity, updated:true})
+  },
+
+  async delete(req,res,next){
+    let id = req.params.id;
+
+    let activity = await Activity.findByPk(id);
+
+    activity.destroy();
+
+    res.redirect('/');
   }
 }
